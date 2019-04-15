95f01a20b659923c482f8f587ce421711056400a
Konstantinos Blounas <kblounas@Persado.local>
2019-04-15 10:45:49 +0300

    updated angular version to latest, fixed dependency issue and status validator updates to required displaying correctly the error message

--- src/app/feature/new-bug/new-bug.component.ts
+++ src/app/feature/new-bug/new-bug.component.ts
@@ -1,7 +1,7 @@
 import { Bug } from 'src/app/core/models/bugs.model';
 import { ApiServiceService } from './../../core/services/api-service.service';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -14,55 +14,44 @@ export class NewBugComponent implements OnInit {
   myForm: FormGroup;
 
   Priority = [
-    {name: 'Minor', value: 1},
-    {name: 'Major', value: 2},
-    {name: 'Critical', value: 3}
+    { name: 'Minor', value: 1 },
+    { name: 'Major', value: 2 },
+    { name: 'Critical', value: 3 }
   ];
 
-  Reporter = [
-    'QA', 'PO', 'DEV'
-  ];
+  Reporter: Array<string> = ['QA', 'PO', 'DEV'];
 
-  Status = [
-    'Ready for Test', 'Done', 'Rejected'
-  ];
+  Status: Array<string> = ['Ready for Test', 'Done', 'Rejected'];
 
-
-  titleFormControl = new FormControl('', [Validators.required]);
-  descriptionFormControl = new FormControl('', [Validators.required]);
-  priorityFormControl = new FormControl('', [Validators.required]);
-  reporterFormControl = new FormControl('', [Validators.required]);
-  statusFormControl = new FormControl('', []);
-
-
-  constructor(private api: ApiServiceService, private router: Router) {
+  constructor(private api: ApiServiceService, private router: Router, private fb: FormBuilder) {
   }
 
   ngOnInit() {
-    this.myForm = new FormGroup({
-      title: this.titleFormControl,
-      description: this.descriptionFormControl,
-      priority: this.priorityFormControl,
-      reporter: this.reporterFormControl,
-      status: this.statusFormControl
+    this.myForm = this.fb.group({
+      title: ['', [Validators.required]],
+      description: ['', [Validators.required]],
+      priority: ['', [Validators.required]],
+      reporter: ['', [Validators.required]],
+      status: ['', []]
     });
-    this.reporterFormControl.valueChanges.subscribe(value => {
+    this.myForm.controls.reporter.valueChanges.subscribe(value => {
       if (value === 'QA') {
-        this.statusFormControl.setValidators(Validators.required);
+        this.myForm.controls.status.setValidators(Validators.required);
+        this.myForm.controls.status.updateValueAndValidity();
       }
     });
   }
 
   formSubmit(myform: FormGroup) {
     const bug: Bug = {
-     title: myform.value.title,
-     description: myform.value.description,
-     priority: myform.value.priority,
-     reporter: myform.value.reporter,
-     status: myform.value.status
+      title: myform.value.title,
+      description: myform.value.description,
+      priority: myform.value.priority,
+      reporter: myform.value.reporter,
+      status: myform.value.status
     };
     if (myform.valid) {
-      this.api.postBug(bug).subscribe( () => this.router.navigate(['/dashboard']));
+      this.api.postBug(bug).subscribe(() => this.router.navigate(['/dashboard']));
     }
   }
 
