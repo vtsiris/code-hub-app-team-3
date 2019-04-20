1382272c1c7b8a3ac7259ca45635ad232a6340ca
Konstantinos Blounas <kblounas@Persado.local>
2019-04-20 16:21:25 +0300

    user story 9 completed

--- src/app/feature/bug-handler/bug-handler.component.spec.ts
+++ src/app/feature/bug-handler/bug-handler.component.spec.ts
@@ -1,7 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { BugHandlerComponent } from './bug-handler.component';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { MaterialModule } from 'src/app/core/modules/material.module';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('NewBugComponent', () => {
   let component: BugHandlerComponent;
@@ -10,7 +13,7 @@ describe('NewBugComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [BugHandlerComponent],
-      imports: [ReactiveFormsModule]
+      imports: [ReactiveFormsModule, MaterialModule, HttpClientModule, RouterTestingModule, BrowserAnimationsModule],
     }).compileComponents();
   }));
 
@@ -18,5 +21,41 @@ describe('NewBugComponent', () => {
     fixture = TestBed.createComponent(BugHandlerComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  fdescribe('User story 9', () => {
+    it('Should validate that the form is invalid if no data entered', () => {
+      component.ngOnInit();
+      expect(component.myForm.invalid).toBeTruthy();
+    });
+
+    it('Should validate that the form is valid by having the minimum required fields', () => {
+      component.ngOnInit();
+      component.myForm.controls.title.setValue('aaa');
+      component.myForm.controls.description.setValue('a');
+      component.myForm.controls.reporter.setValue('PO');
+      component.myForm.controls.priority.setValue(1);
+      expect(component.myForm.valid).toBeTruthy();
+    });
+
+    it('Should set the Reporter to “QA” and validate that the form is invalid', () => {
+      component.ngOnInit();
+      component.myForm.controls.title.setValue('aaa');
+      component.myForm.controls.description.setValue('a');
+      component.myForm.controls.reporter.setValue('QA');
+      component.myForm.controls.priority.setValue(1);
+      expect(component.myForm.invalid).toBeTruthy();
+    });
+
+    it('Should set the Reporter to “QA”, set any Status and validate that the form is valid', () => {
+      component.ngOnInit();
+      component.myForm.controls.title.setValue('aaa');
+      component.myForm.controls.description.setValue('a');
+      component.myForm.controls.priority.setValue(1);
+      component.myForm.controls.reporter.setValue('QA');
+      component.myForm.controls.status.setValue('Done');
+      expect(component.myForm.valid).toBeTruthy();
+    });
+
   });
 });
--- src/app/feature/resolvers/bugs-resolver.service.spec.ts
+++ src/app/feature/resolvers/bugs-resolver.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { BugsResolverService } from './bugs-resolver.service';
+import { BugsResolver } from './bugs-resolver.service';
 
 describe('BugsResolverService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: BugsResolverService = TestBed.get(BugsResolverService);
+    const service: BugsResolver = TestBed.get(BugsResolver);
     expect(service).toBeTruthy();
   });
 });
